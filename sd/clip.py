916b7b72d8c7fe0b2f988d5e779370442ddc5689
Newbyl <nabyl.qgn@gmail.com>
2024-08-27 21:35:16 +0200

    finished

--- sd/clip.py
+++ sd/clip.py
@@ -3,60 +3,63 @@ from torch import nn
 from torch.nn import functional as F
 from attention import SelfAttention
 
+class CLIPEmbedding(nn.Module):
+    def __init__(self, n_vocab: int, n_embd: int, n_token: int):
+        super().__init__()
+        
+        self.token_embedding = nn.Embedding(n_vocab, n_embd)
+        self.position_embedding = nn.Parameter(torch.zeros((n_token, n_embd)))
+    
+    def forward(self, tokens):
+        x = self.token_embedding(tokens)
+        x += self.position_embedding
+        
+        return x
+
+class CLIPLayer(nn.Module):
+    def __init__(self, n_head: int, n_embd: int):
+        super().__init__()
+        
+        self.layernorm_1 = nn.LayerNorm(n_embd)
+        self.attention = SelfAttention(n_head, n_embd)
+        self.layernorm_2 = nn.LayerNorm(n_embd)
+        self.linear_1 = nn.Linear(n_embd, 4 * n_embd)
+        self.linear_2 = nn.Linear(4 * n_embd, n_embd)
+
+    def forward(self, x):
+        residue = x
+        
+        x = self.layernorm_1(x)
+        x = self.attention(x, causal_mask=True)
+        x += residue
+
+        residue = x
+        x = self.layernorm_2(x)
+        x = self.linear_1(x)
+        x = x * torch.sigmoid(1.702 * x)
+        x = self.linear_2(x)
+        x += residue
+
+        return x
+
 class CLIP(nn.Module):
     def __init__(self):
+        super().__init__()
         self.embedding = CLIPEmbedding(49408, 768, 77)
-        self.layers = nn.Module([
+
+        self.layers = nn.ModuleList([
             CLIPLayer(12, 768) for i in range(12)
         ])
 
-        self.layer_norm = nn.LayerNorm(768)
-        
-    def forward(self, tokens):
-        tokens = tokens.type(torch.long)
-        state = self.embedding(tokens)
-        
-        for layer in self.layers:
-            state = layer(state)
-        
-        return self.layer_norm(state)
-
-
-class CLIPEmbedding(nn.Module):
-    def __init__(self, vocab_size, embed_dim, n_tokens):
-        super().__init__()
-        
-        self.token_embedding = nn.Embedding(vocab_size, embed_dim)
-        self.positional_embedding = nn.Parameter(torch.zeros(n_tokens, embed_dim))
-        
+        self.layernorm = nn.LayerNorm(768)
     
-    def forward(self, tokens):
-        return self.token_embedding(tokens) + self.positional_embedding
+    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
+        tokens = tokens.type(torch.long)
+        
+        state = self.embedding(tokens)
 
-
-class CLIPLayer(nn.Module):
-    def __init__(self, n_heads, embed_dim):
-        super().__init__()
-        self.layernorm_1 = nn.LayerNorm(embed_dim)
-        self.attention = SelfAttention(n_heads, embed_dim)
-        self.layernorm_2 = nn.LayerNorm(embed_dim)
+        for layer in self.layers: 
+            state = layer(state)
+        output = self.layernorm(state)
         
-        self.linear_1 = nn.Linear(embed_dim, embed_dim * 4)
-        self.linear_2 = nn.Linear(embed_dim * 4, embed_dim)
-        
-    def forward(self, x):
-        res = x
-        
-        # Self-attention
-        x = self.layernorm_1(x)
-        x = self.attention(x, causal_mask=True)
-        x = x + res
-        
-        # FFN
-        res = x
-        x = self.layernorm_2(x)
-        x = self.linear_1(x)
-        x = x * torch.sigmoid(x * 1.702) # quickgelu activation
-        x = self.linear_2(x)
-        
-        return x + res
+        return output
